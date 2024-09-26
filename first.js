16f768859f0e9bf311e69994bb0b13d2760350c4
aarthyacchu <aarthyacchu595@gmail.com>
2024-09-26 22:03:27 +0530

    first commit

---
+++ first.js
@@ -0,0 +1,98 @@
+// CHAPTER ONE
+
+//Practice Question 1
+// Create a const object called “product” to store information  (product name, rateings, price, offers)
+
+// const  product = {
+//     productName : "Pen",
+//     ratings : 4,
+//     price : 250,
+//     offer : 5,
+// };
+
+//Practice Question 1
+// Create a const object called “profile” to store information (username, userID, followers, following, posts)
+
+// const user = {
+//     userID : "Aarthyacchu",
+//     userName : "Aarthy",
+//     posts : 24,
+//     followers : 600,
+//     following : 4
+// };
+
+
+// CHAPTER TWO
+
+//Practice Question 1
+//  Get user to input a number using prompt(“Enter a number”). Check if the number is a multiple of 5 or not.
+
+// let n = prompt("Enter a number");
+
+// if (n%5==0){
+//     console.log(n, " is divisible by 5");
+// }
+// else{
+//     console.log(n, " not divisible by 5");
+// }
+
+//Practice Question 2
+
+// let marks = prompt("Enter the marks");
+
+// if ( marks >=80 ){
+//     console.log("A")
+// }
+// else if( marks >=70){
+//     console.log("B")
+// }
+// else if( marks >=60){
+//     console.log("C")
+// }
+// else if( marks >=50){
+//     console.log("D")
+// }
+// else{
+//     console.log("F")
+// }
+
+// CHAPTER THREE
+
+//Practice Question 1
+// Print all even numbers from 0 to 100.
+
+// for (let i = 0; i <= 100; i++){
+//     if (i % 2 === 0){
+//         console.log(i);
+//     }
+// }
+
+
+//Practice Question 2
+//Create a game where you start with any random game number. Ask the user to keep guessing the game number until the user enters correct value.
+
+// let guess = 25;
+
+// let number = prompt ("Guess the number ");
+
+// // console.log(number);
+
+// if(guess==number){
+//     console.log("Congragulations you have guessed the right number");
+// }
+// else{
+//     console.log("Sorry you have guessed the wrong number. Try again")
+// }
+
+//Practice Question 3
+//Prompt the user to enter their full name. Generate a username for them based on the input. Start username with @, followed by their full name and ending with the fullname length.
+
+// let userName = prompt ("Enter your User name");
+// str = userName.length
+
+// userId = ` @${userName}${str}`
+// console.log(userId)
+
+// CHAPTER FOUR
+
+//Practice Question 1
