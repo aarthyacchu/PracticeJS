8055d7db9abb98959b2f32d6a3fa0178be1aca6c
aarthyacchu <aarthyacchu595@gmail.com>
2024-09-27 22:43:38 +0530

    commit

--- first.js
+++ first.js
@@ -96,3 +96,138 @@
 // CHAPTER FOUR
 
 //Practice Question 1
+//For a given array with marks of students -> [85, 97, 44, 37, 76, 60] . Find the average marks of the entire class.
+
+// let marks = [85, 97, 44, 37, 76, 60];
+
+// let sum = 0;
+
+// for (let i=0; i<marks.length; i++){
+//     sum += marks[i];
+// }
+// avg = sum / 6;
+// console.log(avg);
+
+//Practice Question 2
+// For a given array with prices of 5 items -> [250, 645, 300, 900, 50] All items have an offer of 10% OFF on them. Change the array to store final price after applying offer.
+
+// let price = [250, 645, 300, 900, 50];
+
+// for (let i = 0; i < price.length; i++){
+//     let offer = price[i] / 10;
+//     price[i]  -= offer;
+// }
+
+// console.log(price);  
+
+//Practice Question 3
+// Create an array to store companies -> “Bloomberg,“Microsoft” ,“Uber”, “Google” , “IBM”, “Netflix”
+
+// let companies = [ "Bloomberg" , "Microsoft","Uber","Google","IBM","Netflix"];
+
+// A:  Remove the first companyfrom the array
+
+// companies.shift();
+// console.log(companies)
+
+// B: Remove Uber & Add Ola in its place
+
+// cmp = companies.splice(1,1,"Ola");
+// console.log(cmp)
+// console.log(companies);
+
+//  C: Add Amazon at the end
+
+// companies.push("Amazon");
+// console.log(companies);
+
+// CHAPTER FIVE
+
+//Practice Question 1
+//Create a function using the “function” keyword that takes a String as an argument & returns the number of vowels in the string.
+
+
+// function vowels(msg){
+
+//     let myString = ["a", "e", "i", "o", "u"];
+//     let count = 0;
+
+//     for(let val of msg){
+
+//         if (myString.includes(val)){
+//             count++;
+//         }
+//     }
+//     return count;
+// }
+
+// console.log(vowels("aishwarya"));
+
+//Practice Question 2
+//Create a arrow function that takes a String as an argument & returns the number of vowels in the string.
+
+// const vowels = (msg) => {
+
+    
+//     let myString = ["a", "e", "i", "o", "u"];
+//     let count = 0;
+
+//     for(let val of msg){
+        
+//         if (myString.includes(val)){
+//             count++;
+//         }
+//     }
+//     return count;
+// }
+
+// console.log(vowels("ball"));
+
+//Practice Question 3
+// For a given array of numbers, print the square of each value using the forEach loop.
+
+// let arr = [1, 2, 3, 4, 5, 6,7, 8, 9, 10];
+
+// arr.forEach( (val) =>{
+//     let sqr = val * val;
+//     console.log(sqr);
+// })
+
+//Practice Question 4
+
+//We are given array of marks of students. Filter our of the marks of students that scored 90+.
+
+// let myArray = [67, 89, 67, 95, 93, 62, 23, 67, 34, 89, 95, 85, 95, 98, 93, 45, 56, 98];
+
+// newArray = myArray.filter( ( val ) => {
+
+//     return val >= 90;
+
+// })
+
+// console.log(newArray);
+
+//Practice Question 4
+// Take a number n as input from user. Create an array of numbers from 1 to n.
+//Use the reduce method to calculate sum of all numbers in the array.
+//Use the reduce method to calculate product of all numbers in the array.
+
+let input = prompt("Enter a number");
+
+let array = [ ];
+
+for(let i = 1; i<=input; i++){
+    array[i-1] = i;
+}
+console.log(array);
+
+const sum = array.reduce(
+    (initial, end) => initial + end,
+);
+console.log("The sum of entered number is", sum);
+
+const  pdt = array.reduce(
+    (initial, end) => initial * end,
+);
+
+console.log("The product of entered number is", pdt);
